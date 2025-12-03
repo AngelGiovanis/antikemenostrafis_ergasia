781c48902ce3086770f58c3d6fc71a8b6532f0ff
AngelGiovanis <116574977+AngelGiovanis@users.noreply.github.com>
2025-12-03 16:15:49 +0200

    txt file

---
+++ new_simulation/types.h
@@ -0,0 +1,21 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+class world(){
+    public:
+    private:
+}
+
+class grid_world(){
+    private:
+        int height;
+        int width;
+        char **map;
+    public:
+        gridworld(); 
+        void make_map();
+        
+}
+
+
+#endif
